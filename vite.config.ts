d9528dea454f1d6e9c43f88cb912e4086121c17a
YernarT <toktaryernar@gmail.com>
2023-06-28 15:20:15 +0600

    chore: :wrench: remove less dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from "vite";
 
 import path from "path";
-import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(path.resolve(), "./src"),
-    },
-  },
-
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer({
-          overrideBrowserslist: "last 2 versions",
-        }),
-      ],
     },
   },
 });
