a8e19229bb4855afe85dd06edf26164cbcdcea3f
YernarT <toktaryernar@gmail.com>
2023-07-21 22:58:46 +0600

    init: :rainbow: 类型定义

---
+++ src/modules/dto.ts
@@ -0,0 +1,26 @@
+import Block from "@/modules/block";
+
+export type T_ScreenSize = {
+  screenWidth: number;
+  screenHeight: number;
+};
+
+export type T_DOM_Position = {
+  transformX: number;
+  transformY: number;
+};
+
+export type T_MAP_Position = {
+  row: number;
+  column: number;
+};
+
+export type T_Direction = "TOP" | "BOTTOM" | "LEFT" | "RIGHT";
+
+export type T_GameStatus = "READY" | "PAUSE" | "RUNNING" | "WIN" | "FAIL";
+
+export type T_SnakeNode = {
+  block: Block;
+  previousPosition: T_MAP_Position;
+  currentPosition: T_MAP_Position;
+};
