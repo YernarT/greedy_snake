694710050df97a3cd060cee865827ea052b73e76
YernarT <toktaryernar@gmail.com>
2024-07-08 22:36:14 +0500

    update: :hammer: 移除冗余类型

--- src/modules/dto.ts
+++ src/modules/dto.ts
@@ -1,28 +1,24 @@
-import Block from "@/modules/block";
-
 export type T_ScreenSize = {
   screenWidth: number;
   screenHeight: number;
 };
 
-export type T_DOM_Position = {
-  transformX: number;
-  transformY: number;
+export type T_Position = {
+  x: number;
+  y: number;
 };
 
-export type T_MAP_Position = {
-  row: number;
-  column: number;
-};
-
-export type T_Direction = "RIGHT" | "BOTTOM" | "LEFT" | "TOP";
+export type T_Direction = "NULL" | "RIGHT" | "BOTTOM" | "LEFT" | "TOP";
 
 export type T_GameStatus = "READY" | "PAUSE" | "RUNNING" | "WIN" | "FAIL";
 
 export type T_SnakeNode = {
-  block: Block;
-  previousPosition: T_MAP_Position;
-  currentPosition: T_MAP_Position;
+  // block: Block;
+  previousPosition: T_Position;
+  currentPosition: T_Position;
 };
 
-export type T_Task = CallableFunction;
+export type T_Task = {
+  taskId: string;
+  renderFunction: CallableFunction;
+};
